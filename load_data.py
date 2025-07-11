25717d681a26e8b658e3e1558cde3382cc3f92f1
743211258 <140025507+743211258@users.noreply.github.com>
2025-07-11 02:42:43 -0400

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,6 +1,7 @@
 # load_data.py
 import torch
 import json
+import random
 from input import board_to_tensor
 from game import move_id, move_to_index
 import chess
@@ -12,18 +13,17 @@ def recover_policy(pi_indices, pi_probs, total_moves=3820):
             policy[idx] = prob
     return policy
 
-def load_training_data(filename="training_data.json"):
+def load_random_sample(filename="training_data.jsonl"):
     with open(filename, 'r') as f:
-        raw_data = json.load(f)
+        lines = f.readlines()
+    line = random.choice(lines)
+    item = json.loads(line)
 
-    data = []
-    for item in raw_data:
-        board = chess.Board(item["state_fen"])
-        state_tensor = board_to_tensor(board)
-        pi_indices = item["pi_indices"]
-        pi_probs = item["pi_probs"]
-        z = item["z"]
-        policy_tensor = recover_policy(pi_indices, pi_probs)
-        data.append((state_tensor, policy_tensor, z))
+    board = chess.Board(item["state_fen"])
+    state_tensor = board_to_tensor(board)
+    pi_indices = item["pi_indices"]
+    pi_probs = item["pi_probs"]
+    z = item["z"]
+    policy_tensor = recover_policy(pi_indices, pi_probs)
+    return state_tensor, policy_tensor, z
 
-    return data
